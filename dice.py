107147594fa3dd0e6bc2d5b56e73e4407a03581f
FABULOUS KILLJOY <hutt2345@gmail.com>
2014-04-03 20:43:09 -0400

    This update changes the program from asking the user how many sides they would like on a dice to asking how many sides in addition to asking th e number of dice the user would like to roll. Other minor issues were also changed, such as small syntax and spacing errors.

--- dice.py
+++ dice.py
@@ -1,6 +1,7 @@
 print "The result of the dice roll is", random.randint
 
-print "How many sides should the dice have?"
+print "How many dice would you like to roll? Also, how many sides would you like for each dice to have?"
+dice = raw_input()
 sides = raw_input()
 print random.randint(1,sides)
-
+print random.randint(1,dice)
