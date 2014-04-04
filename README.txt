6f433fa7ef4dad969bd292f35e3eab699db96a64
FABULOUS KILLJOY <hutt2345@gmail.com>
2014-04-03 20:22:42 -0400

    I have added a file describing the program and a TextWrangler document containing the inital coding of the project. This has completed the first two steps of the assignment for Week 8 in the programming elective at PRISMS.

---
+++ README.txt
@@ -0,0 +1,1 @@
+This program simulates the rolling of a dice. 
---
+++ dice.py
@@ -0,0 +1,6 @@
+print "The result of the dice roll is", random.randint
+
+print "How many sides should the dice have?"
+sides = raw_input()
+print random.randint(1,sides)
+
